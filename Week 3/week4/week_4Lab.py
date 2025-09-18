41d74c5560f2e367f58a6c93e0c146100b5a5431
William Tauferner <112906919+Will-Tauferner@users.noreply.github.com>
2025-09-18 12:21:18 -0500

    Update week_4Lab.py

--- Week 3/week4/week_4Lab.py
+++ Week 3/week4/week_4Lab.py
@@ -14,13 +14,33 @@ print(f'Number of times halved: {num}')
 
 '''
 #2
-
+'''''
 word = input('Enter a word: ')
-
-for i in range(1 , len(word) - 1,) :
+result = ''
+# First start point, Ending point, step size
+for i in range(1 , len(word) - 1,2) :
     result += word[i]
 
 print(f"Output = {result}")
+'''
+#3 
+#Using a loop, write a program that prints every even number between 37 and 1050 (inclusively)
+'''''
+x = 37 
+y = 1050
+for num in range (x, y)
+    x += 1
+    print(x)
+'''''
+#4 
+word = ''
+while True:
+    #Read the user input
+    user_in = input('Enter a letter: ')
+    #if the user typed 'done' we stop
+    if user_in == 'done':
+        break
+    else:
+        word += user_in
 
-
-
+print(f'The final word is {word}')
