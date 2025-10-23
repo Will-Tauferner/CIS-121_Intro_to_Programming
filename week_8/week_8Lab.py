3d57cac541d9af8886b1b766354134b10d64004d
William Tauferner <112906919+Will-Tauferner@users.noreply.github.com>
2025-10-23 11:20:31 -0500

    Update Week 8

--- week_8/week_8Lab.py
+++ week_8/week_8Lab.py
@@ -1,3 +1,62 @@
+#1
+#Write a function that lets the user guess whether the flip of a coin results in
+#heads or tails. The function randomly generates an integer 0 or 1, which represents head or tail. The
+#function returns if the guess is correct or incorrect. The argument for the function will be guess (the
+#guess of the user, 0 for heads and 1 for tails), if no argument is provided then the default should be
+#0 for heads.
+#Hint: Use the following lines of code to create the function.
+#from random import randint
+#value = randint(0,1) #picks a random integer. Either 0 or 1
+
+from random import randint
+
+def toss_coin(guess=0):
+    value = randint(0, 1)  # 0 = heads, 1 = tails
+    if guess == value:
+        return "Correct!"
+    else:
+        return "Incorrect!"
+print(toss_coin())
+
+#2
+#Write a function that lets the user guess whether a randomly generated number
+#is odd or even. The function randomly generates an integer between 0 and 9 (inclusive) and returns
+#whether the user’s guess is correct or incorrect. The argument for the function will be guess (the user’s
+#guess, either” odd” or” even”), if no argument is provided then the default guess should be even.
+#Hint: Use the following lines of code to create the function.
+#from random import randint
+#value = randint(0,9) #picks a random integer between 0-9 inclusive
+
+from random import randint
+
+def guess(guess="even"):
+    value = randint(0, 9)  # random number between 0–9
+
+    if (value % 2 == 0 and guess == "even") or (value % 2 == 1 and guess == "odd"):
+        return "Correct!"
+    else:
+        return "Incorrect!"
+
+print(guess('odd'))
+
+#3
+#Write a function that returns the number of copies of the same number. The arguments for the func-
+#tion will be num 1 (first number), num 2 (second number), and num 3 (third number), if no argument
+#is provided then the default for all 3 values should be 0
+
+def copies(num1=2, num2=2, num3=0):
+    count = 0
+    
+    if num1 == num2 == num3:
+        count = 3
+    elif num1 == num2 or num1 == num3 or num2 == num3:
+        count = 2
+    else:
+        count = 0
+
+    return count
+print(copies())
+
 #4 
 #Write a function to create a game of Rock, Paper, Scissors. The function will return the winner of the
 #game played by two players. The arguments to the function will be player1 (the first player’s choice)
@@ -16,7 +75,42 @@ def find_winner(p1 = 'rock',p2 = 'rock'):
         return 'Player 1 wins'
     else:
         return 'player 2 wins'
-print(find_winner('rock', 'paper'))
+print(find_winner())
+
+#5
+#Luke Skywalker has friends and family, but he is getting older and having trouble remembering them
+#all. Write a function that will return the relation defined in the table below. The arguments to
+#the function will be name (name of the person related to Luke), if no argument is provided then the
+#default should be nothing. That is, the empty word
+
+def find_relation(name):
+    if name == 'Darth Vader':
+        return 'Father'
+    elif name == 'Leia':
+        return 'Sister'
+    elif name == 'Han':
+        return 'Brother in Law'
+    elif name == 'R2D2':
+        return 'Droid'
+    else:
+        return 'nothing'
+print(find_relation('Darth Vader'))
+
+#7
+#Write a function that takes 3 numbers as arguments, num 1 (first number), num 2 (second number),
+#and num 3 (third number). num 1 should be mandatory. If no arguments are provided for num 2 or
+#num 3 then use 5 for num 2 and 25 for num 3. Return a list of the integers in ascending order.
+
+def number(num1=0,num2=5,num3=35):
+    if num1 > num2:
+        num1, num2 = num1 , num2
+    if num1 > num3:
+        num1, num3 = num1, num3
+    if num2 > num3:
+        num2, num3 = num2, num3 
+    return[num1,num2,num3]
+print(number())
+
 
 #8 
 #Write a function that takes 3 numbers as arguments, num 1 (first number), num 2 (second number),
@@ -33,6 +127,88 @@ def numbers(num1 = 20,num2 = 15,num3 = 5):
         num2, num3 = num3, num2
     return[num1,num2,num3]
 print(numbers())
+
+#9
+#Write a function that takes two arguments, a list and a value. The function should return the indices
+#of all occurrences of the value in the list, if no argument is provided then the default should be to
+#find 0.
+
+def get_indices(lst, value=0):
+    indices = []
+    for i in range(len(lst)):
+        if lst[i] == value:
+            indices.append(i)
+    return indices
+print(get_indices([1,3,4,0,5,6]))
+
+#10
+#Write a function that returns the factors of a given integer. The argument of the function will be
+#num (integer to find factors for), if no argument is provided then the default should be 36
+
+def factors(num=36):
+    factor_list = []
+    for i in range(1, num + 1):
+        if num % i == 0:
+            factor_list.append(i)
+    return factor_list
+
+print(factors())
+
+#11
+#Write a function that takes two numbers as arguments num and length and returns a list of multiples
+#of num until the list length reaches length, if no argument is provided then the default for the list
+#length should be 5.
+
+def multiples(num, length=5):
+    result = []
+    for i in range(1, length + 1):
+        result.append(num * i)
+    return result
+
+print(multiples(2,))
+
+#12
+def is_even(num):
+    return num % 2 == 0
+def report_evens(numbers):
+    even_list = []
+    for num in numbers:
+        if is_even(num):
+            even_list.append(num)
+    return even_list
+print (report_evens([1,2,4,5,6,7,8]))
+
+#13
+#Write a function named is vowel that returns a boolean value which determines if an letter is a vowel.
+#Write a second function named report vowels that takes a string and returns a list containing all the
+#vowels from the original string. Call the is vowel function as part of the report vowels function.
+#Hint: In the English language, the letters a, e, i, o, and u are the vowels
+
+def is_vowel(letter):
+    return letter.lower() in ['a', 'e', 'i', 'o', 'u']
+
+def report_vowels(text):
+    vowels = []
+    for ch in text:
+        if is_vowel(ch):
+            vowels.append(ch)
+    return vowels
+
+           # True
+print(report_vowels('apple'))  # ['a', 'e']
+
+#14. 
+def is_two_digit_number(num):
+    return (10 <= (num) <= 99)
+def report_two_digit_numbers(numbers):
+    two_digits = []
+    for num in numbers:
+        if is_two_digit_number(num):
+            two_digits.append(num)
+    return two_digits
+print(is_two_digit_number(40))
+
+
 
 #15
 #Write a function named is negative that returns a boolean value which determines if an integer is
---
+++ week_8/week_9lab.py
@@ -0,0 +1,43 @@
+'''
+dog1_name = 'spot'
+dog1_age = 
+dog1_weight = 
+dog1_breed = 
+dog1_size = 
+'''
+'''
+class Classname:
+    def method1():
+        #block of code
+    def method2():
+        #block of code
+'''
+#The first method that many classes have is called a constructor, which defines the way the object is created
+#The constructor can create instance variable to hold the values of an instance of the object class.  
+
+#In Python, the constructor is called __init__().
+
+#ALl methods (in python) require one special parameter (the first parameter) which refers to the object that is being created.
+
+#lets see an example of a planet class that has a 
+#1. name
+#2. radius
+#3. mass
+#4. distance
+'''
+class Planet:
+    def __init__(self, _name):
+        self.name = _nameplanet1 = 
+planet1 = Planet('x25')
+'''
+#Lets see an example of a dog class that has a 
+#1name
+#2 age 
+#3 Breed 
+
+class Dog:
+    def __init__(self, name, age, breed):
+        self.name = name
+        self.age = age
+        self.breed = breed
+    print(__init__('banzai',3,'shiba'))
